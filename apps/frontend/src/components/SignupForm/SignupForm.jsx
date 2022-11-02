3e654edc3d17261db0a510c5102d5eaccb1dcbe6
Sergey Perestoronin <se.gennp@gmail.com>
2022-11-02 18:11:09 +0300

    fix: Add login sign up function

--- apps/frontend/src/components/SignupForm/SignupForm.jsx
+++ apps/frontend/src/components/SignupForm/SignupForm.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Paper, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '@constants/frontend';
-
 import styles from './SignupForm.module.scss';
+import AuthController from '../../controllers/AuthController';
+import { nanoid } from 'nanoid';
 
 const SignupForm = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+
+  const handleEmailChange = (evt) => {
+    setEmail(evt.target.value);
+  }
+  const handlePassChange = (evt) => {
+    setPassword(evt.target.value);
+  }
+  const handlePassChange2 = (evt) => {
+    setPassword2(evt.target.value);
+  }
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    if (password === password2) {
+      const username = email.split('@')[0] + nanoid(10);
+      try {
+        await AuthController.registration(username, email, password);
+      } catch {
+        console.log('Registration failed');
+      }
+      setPassword('');
+      setPassword2('');
+      setEmail('');
+    } else {
+      console.log('Passwords not equal');
+    }
+  }
+
   return (
     <Stack className={styles.signupSection}>
       <Paper elevation={3} className={styles.signupPaper}>
@@ -23,7 +54,7 @@ const SignupForm = (props) => {
         </Link>
 
         <form
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           className={styles.signupForm}
         >
           <h3>Создайте свой аккаунт</h3>
@@ -32,8 +63,8 @@ const SignupForm = (props) => {
               placeholder="E-mail"
               name="email"
               type="email"
-              // onChange={handleEmailChange}
-              // value={email}
+              onChange={handleEmailChange}
+              value={email}
               className={styles.signupInput}
             />
 
@@ -41,17 +72,17 @@ const SignupForm = (props) => {
               placeholder="Придумайте пароль"
               name="password"
               type="password"
-              // onChange={handlePassChange}
-              // value={password}
+              onChange={handlePassChange}
+              value={password}
               className={styles.signupInput}
             />
 
             <input
               placeholder="Повторите пароль"
-              name="password"
+              name="password2"
               type="password"
-              // onChange={handlePassChange}
-              // value={password}
+              onChange={handlePassChange2}
+              value={password2}
               className={styles.signupInput}
             />
           </Stack>
