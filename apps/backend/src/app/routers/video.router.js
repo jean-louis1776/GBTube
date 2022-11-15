530c5f453318133aeac9381412d6a8f0edd704b7
lae1965 <lae1965@yandex.ru>
2022-11-15 11:01:01 +0300

    fix: fix some bags

--- apps/backend/src/app/routers/video.router.js
+++ apps/backend/src/app/routers/video.router.js
@@ -4,7 +4,7 @@ import videoController from '../controllers/video.controller.js';
 
 const router = new Router();
 
-router.post('/upload', (...args) => videoController.upload(...args));
+router.post('/create', (...args) => videoController.upload(...args));
 router.put('/', /*edit*/);
 router.get('/download/:videoName', (...args) => videoController.download(...args));
 router.get('/', /*getAll*/ );
