9e7a685d3779689fe743d119692532685b4b7e6f
Sergey Perestoronin <se.gennp@gmail.com>
2022-11-24 13:36:40 +0300

    feat: Add render playlists`s info and delete button in VideoGrid
    Added render title and description of playlist in VideoGrid component.
    Added delete current playlist button.

--- apps/frontend/src/components/VideoGrid/VideoGrid.jsx
+++ apps/frontend/src/components/VideoGrid/VideoGrid.jsx
@@ -5,7 +5,8 @@ import { Loader } from '../';
 import GetChildrenController from '../../controllers/GetChildrenController';
 
 import { /*useLocation,*/ useNavigate, useParams } from 'react-router-dom';
-import { VIDEO } from '@constants/frontend';
+import { PLAYLIST, VIDEO } from '@constants/frontend';
+import EditItemController from '../../controllers/EditItemController';
 
 const VideoGrid = () => {
   const { idList } = useParams();
@@ -15,6 +16,7 @@ const VideoGrid = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      const {title, description} = await GetChildrenController.getItemById(PLAYLIST, idList.split('_').at(-1));
       console.log(VIDEO, idList);
       const children = await GetChildrenController.getAllItemsById(VIDEO, idList.split('_').at(-1));
       console.log(children);
@@ -22,19 +24,23 @@ const VideoGrid = () => {
         setContent(<p style={{color: 'white'}}>Не создано ни одного видео </p>);
       } else {
         setContent(
-          <Stack
-            direction={/*direction ||*/ 'row'}
-            flexWrap="wrap"
-            justifyContent="start"
-            alignItems="start"
-            gap={2}
-          >
-            {children.map((item, idx) => (
-              <Box key={idx}>
-                <p style={{color: 'white'}}>Видео {item.title}</p>
-              </Box>
-            ))}
-          </Stack>
+          <Box>
+            <p style={{color: 'white'}}>Название текущего плейлиста: {title}</p>
+            <p style={{color: 'white'}}>Описание текущего плейлиста: {description}</p>
+            <Stack
+              direction={/*direction ||*/ 'row'}
+              flexWrap="wrap"
+              justifyContent="start"
+              alignItems="start"
+              gap={2}
+            >
+              {children.map((item, idx) => (
+                <Box key={idx}>
+                  <p style={{color: 'white'}}>Видео {item.title}</p>
+                </Box>
+              ))}
+            </Stack>
+          </Box>
         );
       }
     }
@@ -48,11 +54,21 @@ const VideoGrid = () => {
     navigate(`/${VIDEO}/create/${idList}`/*, { state: { idList: location.state.idList } }*/);
   }
 
+  const handleDeletePlaylist = async () => {
+    console.log(idList, idList.split('_').slice(0, -1).join('_'));
+    try {
+      await EditItemController.deleteItem(PLAYLIST, idList.split('_').at(-1));
+      navigate(`/${PLAYLIST}/get_all/${idList.split('_').slice(0, -1).join('_')}`);
+    } catch(err) {
+      console.log('delete playlist fail',err);
+    }
+  }
+
   return (
     <>
-      <p style={{color: 'white'}}>Видео плейлиста ID: {idList}</p>
       {content}
       <Button onClick={handleCreateChild}>Создать {VIDEO}</Button>
+      <Button onClick={handleDeletePlaylist}>Удалить текущий {PLAYLIST}</Button>
       {/*<Link to={`/${childrenType}/create`}>Создать {childrenType}</Link>*/}
     </>
   );
