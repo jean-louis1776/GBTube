a6c88e0af03bc46b3c3f2bb975422b1b4ba9767a
Ilya Aleksin <aleksin095@gmail.com>
2022-12-02 16:16:29 +0300

    feat: update background fpr History page, build Likes page

--- apps/frontend/src/components/Likes/Likes.jsx
+++ apps/frontend/src/components/Likes/Likes.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
-import { Header, Navbar } from '../';
+import { Header } from '../';
+import VideoListItem from '../VideoListItem/VideoListItem';
+import VideoCard from '../VideoCard/VideoCard';
 
 import styles from './Likes.module.scss';
 
 const Likes = (props) => {
   const selectedCategory = 'Понравившиеся';
 
+  const listId = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
   return (
-    <Box sx={{ display: 'flex', pt: 8 }}>
+    <>
       <Helmet>
         <title>GeekTube | Понравившиеся</title>
       </Helmet>
 
       <Header selectedCategory={selectedCategory} />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          bgcolor: 'darkBackground.main',
-          p: 2,
-          maxHeight: '93vh',
-          flex: 2,
-        }}
-      >
-        <Typography>Likes</Typography>
+
+      <Box className={styles.likesWrapper}>
+        <Box component="main" className={styles.likesMain}>
+          <Typography sx={{ mb: 8 }} className={styles.likesTitle} variant="h4">
+            Понравившиеся видео
+          </Typography>
+
+          <Box className={styles.likesGrid}>
+            {listId.map((index) => (
+              <VideoCard key={index} />
+            ))}
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
