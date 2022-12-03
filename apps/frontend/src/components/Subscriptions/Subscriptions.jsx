4713c01706485be8563ad873398c09a420510eac
Ilya Aleksin <aleksin095@gmail.com>
2022-12-03 18:47:19 +0300

    feat: add minimal layout for Subscriptions page

--- apps/frontend/src/components/Subscriptions/Subscriptions.jsx
+++ apps/frontend/src/components/Subscriptions/Subscriptions.jsx
@@ -2,33 +2,43 @@ import React, { useState } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import { Helmet } from 'react-helmet';
 
-import { Header, Navbar } from '../';
+import { Header } from '../';
+import SearchSubForm from './SearchSubForm';
+import VideoCard from '../VideoCard/VideoCard';
 
 import styles from './Subscriptions.module.scss';
 
 const Subscriptions = (props) => {
   const selectedCategory = 'Мои подписки';
 
+  const listId = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
   return (
-    <Box sx={{ display: 'flex', pt: 8 }}>
+    <>
       <Helmet>
         <title>GeekTube | Подписки</title>
       </Helmet>
 
       <Header selectedCategory={selectedCategory} />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          bgcolor: 'darkBackground.main',
-          p: 2,
-          maxHeight: '93vh',
-          flex: 2,
-        }}
-      >
-        <Typography>Subscriptions</Typography>
+
+      <Box className={styles.subWrapper}>
+        <Box component="main" className={styles.subMain}>
+          <Typography className={styles.subTitle} variant="h4">
+            Мои подписки
+          </Typography>
+
+          <SearchSubForm />
+          {/* 
+          <Box className={styles.likesGrid}>
+            {listId.map((index) => (
+              <VideoCard key={index} />
+            ))}
+          </Box> */}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
