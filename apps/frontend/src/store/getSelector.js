cd5ce62c5eeefc9fc1495e58519840c58a8d37bf
Sergey Perestoronin <se.gennp@gmail.com>
2022-12-04 16:46:28 +0300

    fix: Remove unused files.

--- apps/frontend/src/store/getSelector.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-export const getSelector = (key, subKey) =>
-  createSelector(
-    (state) => state,
-    (state) => state?.[key]?.[subKey] ?? null
-  );
