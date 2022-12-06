7de58a978754e17e8c1cbad0608024823764ec04
Sergey Perestoronin <se.gennp@gmail.com>
2022-12-06 18:59:13 +0300

    add: Add CommentController.js

---
+++ apps/frontend/src/controllers/CommentController.js
@@ -0,0 +1,57 @@
+import $authApi from '../api/AuthClient';
+
+export default class CommentController {
+  static async getAllItemsByVideo(videoId) {
+    console.log('getAllCommentsByVideo run');
+    const url = `/comment/videoId/${videoId}`;
+    console.log(url);
+    return (await $authApi.get(url)).data;
+  }
+  static async getInfoAbout(commentId) {
+    const url = `/comment/videoId/id/${commentId}`;
+    return (await $authApi.get(url)).data;
+  }
+  static async add(idList, textContent) {
+    const url = `api/comment/create`;
+    console.log(url);
+    const dto = {idList, description: textContent};
+    console.log(dto);
+    return await $authApi.post(url, dto, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  static async edit(idList, textContent) {
+    const url = `/comment/edit`;
+    console.log(url);
+    const dto = {idList, description: textContent};
+    return await $authApi.patch(url, dto, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  static async delete(commentId) {
+    const url = `/comment/${commentId}`;
+    return await $authApi.delete(url);
+  }
+  static async dislike(commentId, userId) {
+    const url = `/comment/dislike`;
+    const dto = {id: commentId, userId};
+    await $authApi.patch(url, dto, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  static async like(commentId, userId) {
+    const url = `/comment/like`;
+    const dto = {id: commentId, userId};
+    await $authApi.patch(url, dto, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
