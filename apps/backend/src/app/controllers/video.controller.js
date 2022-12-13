03530fdf6c3609eff1fe93af680c5d88e8aff3eb
lae1965 <lae1965@yandex.ru>
2022-12-13 20:04:02 +0300

    fix: fix bags in deleting history

--- apps/backend/src/app/controllers/video.controller.js
+++ apps/backend/src/app/controllers/video.controller.js
@@ -167,8 +167,8 @@ class VideoController {
   async removeOneFromHistory(req, res, next) {
     try {
       validateError(req);
-      const { user_id, video_id} = req.query;
-      return res.status(204).videoService.removeOneFromHistory(+user_id, +video_id);
+      const { user_id, video_id } = req.query;
+      return res.status(204).json(await videoService.removeOneFromHistory(+user_id, +video_id));
     } catch (e) {
       next(e);
     }
@@ -177,7 +177,7 @@ class VideoController {
   async removeAllFromHistory(req, res, next) {
     try {
       validateError(req);
-      return res.status(204).videoService.removeOneFromHistory(+req.params.user_id);
+      return res.status(204).json(await videoService.removeOneFromHistory(+req.params.user_id));
     } catch (e) {
       next(e);
     }
