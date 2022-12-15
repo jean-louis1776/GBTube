44b361b42c059493a690e7bf80fe79cea0f8a4c6
Ilya Aleksin <aleksin095@gmail.com>
2022-12-15 13:59:02 +0300

    feat: add blank title

--- apps/frontend/src/components/edit-item-info/edit-item-info.jsx
+++ apps/frontend/src/components/edit-item-info/edit-item-info.jsx
@@ -2,7 +2,14 @@ import styles from './edit-item-info.module.scss';
 import { Link, useParams } from 'react-router-dom';
 import React, { useEffect, useRef, useState } from 'react';
 import { Paper, Stack, Typography } from '@mui/material';
-import { DESCRIPTION, logo, TITLE, VIDEO } from '@constants/frontend';
+import {
+  CHANNEL,
+  DESCRIPTION,
+  logo,
+  PLAYLIST,
+  TITLE,
+  VIDEO,
+} from '@constants/frontend';
 import FormComp from './FormComp';
 
 /**
@@ -10,26 +17,40 @@ import FormComp from './FormComp';
  */
 export function EditItemInfo({ elemType, sendData, isEdit, getItemById }) {
   const { idList } = useParams();
-  const refDefaultValues = useRef({[TITLE]: '', [DESCRIPTION]: ''});
+  const refDefaultValues = useRef({ [TITLE]: '', [DESCRIPTION]: '' });
   const [isStartRender, setIsStartRender] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const elemId = idList.split('_').at(-1);
-      return elemType === VIDEO ? await getItemById(elemId) : await getItemById(elemType, elemId);
-    }
+      return elemType === VIDEO
+        ? await getItemById(elemId)
+        : await getItemById(elemType, elemId);
+    };
     if (isEdit) {
-      fetchData().then((data) => {
-        const { title, description } = data;
-        refDefaultValues.current[TITLE] = title;
-        refDefaultValues.current[DESCRIPTION] = description;
-        setIsStartRender(true);
-      }).catch((err) => {
-        console.log(`${elemType} not found`);
-        console.log(err);
-      });
+      fetchData()
+        .then((data) => {
+          const { title, description } = data;
+          refDefaultValues.current[TITLE] = title;
+          refDefaultValues.current[DESCRIPTION] = description;
+          setIsStartRender(true);
+        })
+        .catch((err) => {
+          console.log(`${elemType} not found`);
+          console.log(err);
+        });
     }
-  },[]);
+    let elemName = '';
+    if (elemType === CHANNEL) {
+      elemName = 'канала';
+    } else if (elemType === PLAYLIST) {
+      elemName = 'плейлиста';
+    } else if (elemType === VIDEO) {
+      elemName = 'видео';
+    }
+
+    document.title = `Создание / изменение ${elemName} | GeekTube`;
+  }, [elemType]);
 
   return (
     <Stack className={styles.loginSection}>
@@ -57,12 +78,16 @@ export function EditItemInfo({ elemType, sendData, isEdit, getItemById }) {
           </Typography>
         </Link>
 
-        {!isEdit || isStartRender ? <FormComp
-          elemType={elemType}
-          idList={idList}
-          sendData={sendData}
-          defaultValues={refDefaultValues.current}
-        /> : ''}
+        {!isEdit || isStartRender ? (
+          <FormComp
+            elemType={elemType}
+            idList={idList}
+            sendData={sendData}
+            defaultValues={refDefaultValues.current}
+          />
+        ) : (
+          ''
+        )}
 
         <p className={styles.copyright}>
           &copy; {new Date().getFullYear()} GeekTube Team. Все права защищены
